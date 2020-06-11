be3d39c370e817ff66fe82a2257178777a0cf754
mdomonkos <v-mdomonkos@expedia.com>
2020-06-11 09:16:22 +0200

    add various React component creation

---
+++ src/ComponentList.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+var man = { name: 'jack' };
+
+function FunctionComponentFun({ inputData }) {
+  return (
+    <>
+      <h3>Hello {inputData}</h3>
+      <p>{JSON.stringify(this)}</p>
+    </>
+  );
+}
+
+const FunctionComponentConst = ({ inputData }) => (
+  <>
+    <h3>Hello {inputData}</h3>
+    <p>{JSON.stringify(this)}</p>
+  </>
+);
+
+const FunctionComponentFunBind = FunctionComponentFun.bind(man);
+const FunctionComponentConstBind = FunctionComponentConst.bind(man);
+
+class ReactComponent extends React.Component {
+  render() {
+    return <h3>Hello {this.props.inputData}</h3>;
+  }
+}
+
+class ReactPureComponent extends React.PureComponent {
+  render() {
+    return <h3>Hello {this.props.inputData}</h3>;
+  }
+}
+
+function ReactCreateElementAndFun({ inputData }) {
+  return React.createElement('h3', null, `Hello ${inputData}`);
+}
+
+const ReactCreateElementAndConst = ({ inputData }) => {
+  return React.createElement('h3', null, `Hello ${inputData}`);
+};
+
+const ComponentList = () => {
+  return (
+    <>
+      <h2>Use different component creations:</h2>
+      <div style={{ marginLeft: '20px' }}>
+        <FunctionComponentConst inputData='FunctionComponentConst' />
+        <FunctionComponentConstBind inputData='FunctionComponentConstBind' />
+        <FunctionComponentFun inputData='FunctionComponentFun' />
+        <FunctionComponentFunBind inputData='FunctionComponentFunBind' />
+        <ReactComponent inputData='ReactComponent' />
+        <ReactPureComponent inputData='ReactPureComponent' />
+        <ReactCreateElementAndFun inputData='ReactCreateElementAndFun' />
+        <ReactCreateElementAndConst inputData='ReactCreateElementAndConst' />
+      </div>
+    </>
+  );
+};
+
+export default ComponentList;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import style from './index.css';
+import ComponentList from './ComponentList';
 
 var [a, , b] = [1, 2, 3];
 console.log(a);
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <ComponentList />
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
