042365fb593802117050ee2d0703882936924e9a
mdomonkos <v-mdomonkos@expedia.com>
2020-06-12 08:48:05 +0200

    add logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './logo.svg'
 import style from './index.css';
 import ComponentList from './ComponentList';
 
@@ -9,6 +10,7 @@ console.log(a);
 const App = () => {
   return (
     <>
+      <img src={logo} className="App-logo" alt="logo" />
       <h1>Hello React</h1>
       <ComponentList />
     </>
